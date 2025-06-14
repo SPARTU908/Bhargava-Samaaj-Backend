27d9d128645427e11a065e8ea8098debe5582357
SPARTU908 <sparkinwords@gmail.com>
2025-06-14 16:40:24 +0530

    Membership form completed

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,27 +1,53 @@
 const Payment = require("../models/payment");
 
+// const savePayment = async (req, res) => {
+//   try {
+//     const { name, email, mobile, transaction } = req.body;
+//     if (!name || !email || !mobile || !transaction) {
+//       return res.status(400).json({ message: "All fields are required" });
+//     }
+//     const newPayment = new Payment({
+//       name,
+//       email,
+//       mobile,
+//       transaction,
+//     });
+
+//     await newPayment.save();
+//     res
+//       .status(201)
+//       .json({ message: "Payment saved successfully", data: newPayment });
+//   } catch (error) {
+//     console.error("Error saving payment:", error);
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// };
+
+
 const savePayment = async (req, res) => {
   try {
     const { name, email, mobile, transaction } = req.body;
+
     if (!name || !email || !mobile || !transaction) {
       return res.status(400).json({ message: "All fields are required" });
     }
-    const newPayment = new Payment({
-      name,
-      email,
-      mobile,
-      transaction,
-    });
 
-    await newPayment.save();
-    res
-      .status(201)
-      .json({ message: "Payment saved successfully", data: newPayment });
+    const updatedPayment = await Payment.findOneAndUpdate(
+      { email, transaction }, // Search criteria
+      { name, mobile, email, transaction }, // Data to update
+      { new: true, upsert: true } // Return updated doc or create if not found
+    );
+
+    res.status(200).json({
+      message: "Payment saved or updated successfully",
+      data: updatedPayment,
+    });
   } catch (error) {
     console.error("Error saving payment:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
 
 const getPayment = async (req, res) => {
   try {
