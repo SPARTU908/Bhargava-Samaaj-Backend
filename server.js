d1584e8bf5736003daef138f15d7634446c2e382
SPARTU908 <sparkinwords@gmail.com>
2025-07-10 15:59:31 +0530

    recent update

--- server.js
+++ server.js
@@ -433,7 +433,7 @@ app.use((req, res, next) => {
 
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  // res.setHeader("Access-Control-Allow-Credentials", "true");
+ 
 
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
