d5df8aadb68e242745da2881b2ed7adcc1e908e1
SPARTU908 <sparkinwords@gmail.com>
2025-12-19 15:20:56 +0530

    updation in form date schema

--- models/form.js
+++ models/form.js
@@ -11,7 +11,7 @@ const userFormSchema = new mongoose.Schema(
     birthPlace: String,
     height: String,
     weight: String,
-    dob: String,
+    dob: Date,
     bloodGroup: String,
     manglik: String,
     gotra: String,
